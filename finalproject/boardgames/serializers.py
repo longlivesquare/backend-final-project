acdc1a436372096ac753b46911fe05fab86b84f5
Steven Duran <stevenduran@gmail.com>
2021-05-03 19:11:13 -0700

    Installed drf_writable_nested

--- finalproject/boardgames/serializers.py
+++ finalproject/boardgames/serializers.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
-from .models import Boardgame, Designer
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+from .models import Boardgame, Designer, Category
+
+class DesignerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Designer 
+        fields = ('name','id')
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('type',)
 
 class BoardgameSerializer(serializers.ModelSerializer):
     category = serializers.StringRelatedField()
-    designer = serializers.StringRelatedField(many=True)
+    designer = DesignerSerializer(many=True, required=False)
 
     class Meta:
         model = Boardgame
-        fields = ('name', 'year_published','min_players', 'max_players', 'edition', 'category',  'designer')
+        fields = ('pk','name', 'year_published','min_players', 'max_players', 'edition', 'category',  'designer')
+
+    def create(self, validated_data):
+        designer_data = validated_data.pop('designer')
+        boardgame = Boardgame.objects.create(**validated_data)
+        Designer.objects.create(**designer_data)
+        return boardgame
+
